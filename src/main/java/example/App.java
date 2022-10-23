f528a8e9d4b3d426a1076250a76e70172b2144dd
长淇 <linanxuan.lnx@alibaba-inc.com>
2022-10-23 14:14:10 +0800

    2

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -21,6 +21,6 @@ public class App implements StreamRequestHandler, FunctionInitializer {
     @Override
     public void handleRequest(
             InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-        outputStream.write(new String("hello world\n").getBytes());
+        outputStream.write(new String("hello world changed\n").getBytes());
     }
 }
